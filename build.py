76266e4b44480c7bea2edb0750086334e542eeb2
RyuaNerin <admin@ryuar.in>
2020-07-10 16:57:32 +0900

    wildcard 가 올바른 정규식으로 변환하도록 수정

--- build.py
+++ build.py
@@ -1,9 +1,10 @@
 #!/bin/python3
 # -*- coding:utf-8 -*-
 
+import datetime
+import fnmatch
 import io
 import re
-import datetime
 
 with open("hosts.txt", "r", encoding="utf-8") as _fr:
     hosts = _fr.readlines()
@@ -12,10 +13,18 @@ with open("only-stackoverflow.tmpl", "r", encoding="utf-8") as _fr:
     tmpl = _fr.read()
 
 with open("only-stackoverflow.txt", "w", encoding="utf-8") as _fw:
+    regex_list = []
+    for host in hosts:
+        re_str = fnmatch.translate(host.rstrip())
+        re_match = re.match(r'\(\?s:(.*?)\)\\Z', re_str)
+        if re_match is None:
+            regex_list.append(re_str)
+        else:
+            regex_list.append(re_match[1])
+
     _fw.write(
         tmpl.format(
             version=datetime.datetime.now().strftime("%Y.%m.%d.%H.%M"),
-            regex="|".join([re.escape(host.strip("\r\n")).replace('\\*', '*') for host in hosts]).strip('|'),
+            regex="|".join(regex_list).strip('|'),
         )
     )
-
