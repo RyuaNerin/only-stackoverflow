7fbbb17b5be463777ae1017e646fd2bca08b37da
RyuaNerin <admin@ryuar.in>
2020-11-01 21:28:15 +0900

    convert python to bash

---
+++ build.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NOW=`date -u +"%Y.%m.%d %H.%M.%S (UTC)"`
+
+HOST_LIST=`cat hosts.txt | sort -n`
+REGEX_LIST=()
+
+truncate hosts.txt -s 0
+
+while IFS= read -r line; do 
+	if [ -n "$line" ]; then
+		echo "$line" >> hosts.txt
+
+		re=$(echo "$line" | sed 's/\./\\./g; s/\-/\\-/g; s/\*/.\*/g')
+        echo $re
+
+		REGEX_LIST+=($re)
+	fi
+done <<< "$HOST_LIST"
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+REGEX=$(join '|' ${REGEX_LIST[@]})
+
+##################################################
+
+echo "[Adblock Plus 2.0]"                                                                                                > only-stackoverflow.txt
+echo "! Title: only-stackoverflow"                                                                                      >> only-stackoverflow.txt
+echo "! Description: Hide copy of stackoverflow from google and duckduckgo search results. (e.g. stackoverrun)"         >> only-stackoverflow.txt
+echo "! Homepage: https://github.com/RyuaNerin/only-stackoverflow"                                                      >> only-stackoverflow.txt
+echo "! License: https://github.com/RyuaNerin/only-stackoverflow#license"                                               >> only-stackoverflow.txt
+echo "! Expires: 1 days"                                                                                                >> only-stackoverflow.txt
+echo "! Version: ${NOW}"                                                                                                >> only-stackoverflow.txt
+echo ""                                                                                                                 >> only-stackoverflow.txt
+echo "google.com#?#div[role=\"main\"] div#search div[data-async-context] div[data-ved]:-abp-contains(/${REGEX}/)"       >> only-stackoverflow.txt
+echo "google.co.kr#?#div[role=\"main\"] div#search div[data-async-context] div[data-ved]:-abp-contains(/${REGEX}/)"     >> only-stackoverflow.txt
+echo "duckduckgo.com#?#div.result:-abp-contains(/${REGEX}/)"                                                            >> only-stackoverflow.txt
