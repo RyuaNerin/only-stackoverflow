e85f7a014fb9d5b2b6591d2997bd23d83342688e
RyuaNerin <github@ryuar.in>
2021-07-12 16:59:40 +0900

    Update build.bash

--- build.bash
+++ build.bash
@@ -49,7 +49,7 @@ cat <<EOF > only-stackoverflow.txt
 ! License: https://github.com/RyuaNerin/only-stackoverflow#license
 ! Expires: 1 days
 ! Version: ${NOW}
-
+! 
 google.com#?#div[role="main"] div#search div[data-async-context] div[data-ved]:-abp-contains(/${REGEX}/)
 google.co.kr#?#div[role="main"] div#search div[data-async-context] div[data-ved]:-abp-contains(/${REGEX}/)
 duckduckgo.com#?#div.result:-abp-contains(/${REGEX}/)
